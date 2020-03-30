9c148eeb6fd03cb6bf02e260254b9d14edaf3714
wonsub-yun <57588493+wonsub-yun@users.noreply.github.com>
2020-03-30 16:11:02 +0900

    Add files via upload

---
+++ 파이썬 스터디/credentials.py
@@ -0,0 +1,4 @@
+account_sid = "ACe20f3f6527fc212b608241580a67629f"
+auth_token = "fc252107a68e2821cf82d53c9bab35d7"
+my_cell ="+821033957199"
+my_twillio="+12029027615"  
---
+++ 파이썬 스터디/send_sms.py
@@ -0,0 +1,8 @@
+from twilio.rest import Client
+from credentials import account_sid, auth_token, my_cell, my_twillio
+
+client= Client(account_sid, auth_token)
+
+my_msg="Your message goes here..."
+
+message=client.messages.create(to=my_cell, from_=my_twillio, body=my_msg)
